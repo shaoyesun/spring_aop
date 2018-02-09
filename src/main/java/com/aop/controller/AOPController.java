a7cd004957adfaf73189401dcff700cd127af632
SunJianfei <shaoyesun183@sina.com>
2018-02-09 17:47:52 +0800

    dddd

--- src/main/java/com/aop/controller/AOPController.java
+++ src/main/java/com/aop/controller/AOPController.java
@@ -4,7 +4,11 @@ import com.aop.service.AOPService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 /**
  * Created by root on 17-8-3.
@@ -17,7 +21,15 @@ public class AOPController {
     private AOPService aopService;
 
     @RequestMapping(value = "/hello")
-    public String hello() {
+    public String hello(HttpServletRequest req, Model model) {
+        aopService.hello();
+        req.setAttribute("result", "success");
+        model.addAttribute("model", "model");
+        return "index";
+    }
+
+    @RequestMapping(value = "/hello1")
+    public String hello1() {
         aopService.hello();
         return "index";
     }
